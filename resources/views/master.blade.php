84377067f7e635b0a0173a689b8ee2010b7dded5
Masaki Kawabata Neto <kawabata@usp.br>
2020-09-28 11:17:12 -0300

    font awesome já está no theme

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,8 +2,7 @@
 
 @section('styles')
     @parent
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
-    <link rel="stylesheet" href="/css/sites.css">
+    {{-- <link rel="stylesheet" href="/css/sites.css"> --}}
 @stop
 
 @section('javascripts_head')
