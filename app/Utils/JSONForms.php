e95d384af1aa526ecbe82939e42c0d0731b1b9b9
Masaki Kawabata Neto <kawabata@usp.br>
2020-10-10 20:11:48 -0300

    desfazendo campos padrão

--- app/Utils/JSONForms.php
+++ app/Utils/JSONForms.php
@@ -9,26 +9,27 @@ class JSONForms
 {
     public static function generateForm($fila, $chamado = null)
     {
-        $template_default = [
-            'descricao' => [
-                'label' => 'Descrição',
-                'type' => 'textarea',
-            ],
-            'notes' => [
-                'label' => 'Anotações do atendente (oculto para o usuário)',
-                'type' => 'textarea',
-                'can' => 'admin',
-            ],
-        ];
-        if ($fila->template) {
-            $template = json_decode(json_encode(
-                array_merge(json_decode($fila->template, true), $template_default)
-            ));
-        } else {
-            $template = json_decode(json_encode(
-                $template_default
-            ));
-        }
+        // $template_default = [
+        //     'descricao' => [
+        //         'label' => 'Descrição',
+        //         'type' => 'textarea',
+        //     ],
+        //     'notes' => [
+        //         'label' => 'Anotações do atendente (oculto para o usuário)',
+        //         'type' => 'textarea',
+        //         'can' => 'admin',
+        //     ],
+        // ];
+        // if ($fila->template) {
+        //     $template = json_decode(json_encode(
+        //         array_merge(json_decode($fila->template, true), $template_default)
+        //     ));
+        // } else {
+        //     $template = json_decode(json_encode(
+        //         $template_default
+        //     ));
+        // }
+        $template = json_decode($fila->template);
 
         $form = [];
         $data = null;
