17ef31922c395941ca2cad5803dde9f2e1ac31dc
Masaki Kawabata Neto <kawabata@usp.br>
2020-10-11 23:32:14 -0300

    se atribuido vamos colocar usuarios da tabela

--- database/factories/ChamadoFactory.php
+++ database/factories/ChamadoFactory.php
@@ -26,16 +26,31 @@ class ChamadoFactory extends Factory
     {
         $complexidades = Chamado::complexidades();
         $predios = Chamado::predios();
-        $status = Chamado::status();
+        $statuses = Chamado::status();
+        $status = $statuses[array_rand($statuses)];
+        $atribuido_em = $atribuido_para = $triagem_por = $fechado_em = null;
+        if ($status == 'Atribuído') {
+            $atribuido_em = $this->faker->dateTime($max = 'now', $timezone = null);
+            $atribuido_para = User::inRandomOrder()->first()->codpes;
+            $triagem_por = User::inRandomOrder()->first()->codpes;
+        } 
+
+        if ($status == 'Fechado') {
+            $fechado_em = $this->faker->dateTime($max = 'now', $timezone = null);
+        }
             return [
                 'chamado'        =>  $this->faker->sentence,
                 'predio'         =>  $predios[array_rand($predios)],
                 'sala'           =>  $this->faker->randomDigit,
                 'patrimonio'     =>  $this->faker->unique()->numberBetween(10000, 999999),
-                'status'         =>  $status[array_rand($status)],
+                'status'         =>  $status,
                 'complexidade'   =>  $complexidades[array_rand($complexidades)],
                 'user_id'        =>  User::inRandomOrder()->first()->id,
-                'fila_id'        =>  Fila::inRandomOrder()->first()->id
+                'fila_id'        =>  Fila::inRandomOrder()->first()->id,
+                'fechado_em'     =>  $fechado_em,
+                'atribuido_em'     =>  $atribuido_em,
+                'atribuido_para'     =>  $atribuido_para,
+                'triagem_por'     =>  $triagem_por,
             ];
     }
 }
