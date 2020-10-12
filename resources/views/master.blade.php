3568af365c0d619a260e5b4ee45f4107f7bce4f9
Masaki Kawabata Neto <kawabata@usp.br>
2020-10-12 00:45:27 -0300

    reorganizando

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,22 +1,23 @@
 @extends('laravel-usp-theme::master')
 
 @section('styles')
-    @parent
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@1.3.4/dist/select2-bootstrap4.min.css">
-    {{-- <link rel="stylesheet" href="css/sites.css"> --}}
+@parent
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@1.3.4/dist/select2-bootstrap4.min.css">
+{{-- <link rel="stylesheet" href="css/sites.css"> --}}
 @endsection
 
 @section('javascripts_head')
-    @parent
-    <script src="js/atendimento.js"></script>
-    <script src="https://cdn.ckeditor.com/4.12.1/standard/ckeditor.js"></script>
+@parent
+
 @endsection
 
 @section('javascripts_bottom')
-    @parent
+@parent
+<script src="js/atendimento.js"></script>
+<script src="https://cdn.ckeditor.com/4.12.1/standard/ckeditor.js"></script>
 @endsection
 
 @section('content')
-    @include('messages.flash')
-    @include('messages.errors')
+@include('messages.flash')
+@include('messages.errors')
 @endsection
