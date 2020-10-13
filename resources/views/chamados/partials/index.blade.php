ace0c8f2b234b22a3dc646424e3254f4ae748f88
Fernando G. Moura <fgm@sc.usp.br>
2020-10-13 11:18:01 -0300

    lista meus chamados

--- resources/views/chamados/partials/index.blade.php
+++ resources/views/chamados/partials/index.blade.php
@@ -1,41 +1,50 @@
-@section('styles')
-@parent
-<style>
-  table {
-    table-layout: fixed;
-    word-wrap: break-word;
-  }
-</style>
-@stop
-<?php #dd($chamados); ?>
-<div class="table-responsive">
-  {{ $chamados->links() }}
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>Id</th>
-        <th>Fila</th>
-        <th>Status</th>
-        <th>Aberto em</th>
-        <th>Chamado</th>
-        <!-- <th>Prédio</th>
-        <th>Sala</th> -->
-        <th>Ações</th>
-      </tr>
-    </thead>
-    <tbody>
+<div class="row">
+    <div class="col-md-12 form-inline">
+        <span class="h4 mt-2">Meus Chamados</span>
+        @include('partials.datatable-filter-box', ['otable'=>'oTable'])
+    </div>
+</div>
+
+<table class="table table-striped meus-chamados">
+  <thead>
+    <tr>
+      <th>Id</th>
+      <th>Fila</th>
+      <th>Chamado</th>
+      <th>Status</th>
+      <th>Aberto em</th>
+    </tr>
+  </thead>
+  <tbody>
 
     @forelse ($chamados->sortByDesc('created_at') as $chamado)
-      <tr>
-        @include('chamados/partials/chamado')
-      </tr>
+    <tr>
+      <td> #{{ $chamado->id }} </td>
+      <td> ({{ $chamado->fila->setor->sigla }}) {{ $chamado->fila->nome }}</td>
+      <td> <a href="chamados/{{$chamado->id}}"> {!! $chamado->chamado !!} </a></td>
+      <td> <span style="color:red;"> {{ $chamado->status }} </span> </td>
+      <td> {{ Carbon\Carbon::parse($chamado->created_at)->format('d/m/Y H:i') }}</td>
+    </tr>
     @empty
-      <tr>
-        <td colspan="6">Não há chamados</td>
-      </tr>
+    <tr>
+      <td colspan="6">Não há chamados</td>
+    </tr>
     @endforelse
-    
-    </tbody>
-  </table>
-  {{ $chamados->appends(request()->query())->links() }}
-</div>
+
+  </tbody>
+</table>
+
+@section('javascripts_bottom')
+@parent
+<script>
+  $(document).ready(function() {
+
+    oTable = $('.meus-chamados').DataTable({
+      dom: 't',
+      "paging": false,
+      "sort": true
+    });
+
+  })
+</script>
+@endsection
