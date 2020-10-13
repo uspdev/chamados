b4114bbdbe7a96ef9f420b97470f1976daf1519a
Masaki Kawabata Neto <kawabata@usp.br>
2020-10-13 20:58:07 -0300

    habilitando tooltip

--- public/js/atendimento.js
+++ public/js/atendimento.js
@@ -2,4 +2,8 @@ $(function () {
     $(".delete-item").on("click", function(){
         return confirm("Tem certeza que deseja deletar?");
     });
+
+    $('[data-toggle="tooltip"]').tooltip({
+        container: 'body'
+    });
 });
