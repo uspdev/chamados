4e282461617f0fece25a50247d9a563e0b7e42da
Masaki Kawabata Neto <kawabata@usp.br>
2020-10-19 13:51:06 -0300

    necessita ocultar campos de admin

--- resources/views/chamados/show/dados-formulario.blade.php
+++ resources/views/chamados/show/dados-formulario.blade.php
@@ -1,6 +1,14 @@
 <span class="font-weight-bold">Formulário</span><br>
-@foreach($template as $field => $val)
-<span class="text-muted">{{ $val->label }}:</span>
-<span>{{ $extras->$field ?? '' }}</span><br>
-@endforeach
+<div class="ml-2">
+    @foreach($template as $field => $val)
+
+    <span class="text-muted">{{ $val->label }}:</span>
+    <span>{{ $extras->$field ?? '' }}</span>
+    @if(!empty($val->can) && $val->can == 'admin')
+    Este campo so deve aparecer para admin
+    @endif
+    <br>
+    @endforeach
+</div>
+
 <br>
