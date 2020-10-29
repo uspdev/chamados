18c1ead334c0aa7de7f70fcec8eff17c8702e824
Fernando G Moura <fgm@sc.usp.br>
2020-10-29 11:40:58 -0300

    WIP - botões ok

--- resources/views/filas/partials/enable-disable-btn.blade.php
+++ resources/views/filas/partials/enable-disable-btn.blade.php
@@ -21,45 +21,62 @@
     })
 
     function mudaBotaoFila(estado_fila) {
+
         if (estado_fila == 'Em produção') {
             botao = $('.enable-disable-btn').find('[name=producao]');
             botao.removeClass('disabled').removeClass('btn-secondary').addClass('btn-success').addClass('active').blur();
             botao.parent().find('[name=desativada]').removeClass('active').removeClass('btn-danger').addClass('btn-secondary').addClass('disabled');
             botao.parent().find('[name=elaboracao]').removeClass('active').removeClass('btn-warning').addClass('btn-secondary').addClass('disabled');
-        } else if (estado_fila == 'Desativada') {
-            botao = $('.enable-disable-btn').find('[name=desativada]');
-            botao.removeClass('disabled').removeClass('btn-secondary').addClass('btn-danger').addClass('active').blur();
-            botao.parent().find('[name=producao]').removeClass('active').removeClass('btn-success').addClass('btn-secondary').addClass('disabled');
-            botao.parent().find('[name=elaboracao]').removeClass('active').removeClass('btn-warning').addClass('btn-secondary').addClass('disabled');
-        } else { //'Em elaboração'
+        
+        } else if (estado_fila == 'Em elaboração') {
             botao = $('.enable-disable-btn').find('[name=elaboracao]');
             botao.removeClass('disabled').removeClass('btn-secondary').addClass('btn-warning').addClass('active').blur();
             botao.parent().find('[name=producao]').removeClass('active').removeClass('btn-success').addClass('btn-secondary').addClass('disabled');
             botao.parent().find('[name=desativada]').removeClass('active').removeClass('btn-danger').addClass('btn-secondary').addClass('disabled');
+        
+        } else { //'Desativada'
+            botao = $('.enable-disable-btn').find('[name=desativada]');
+            botao.removeClass('disabled').removeClass('btn-secondary').addClass('btn-danger').addClass('active').blur();
+            botao.parent().find('[name=producao]').removeClass('active').removeClass('btn-success').addClass('btn-secondary').addClass('disabled');
+            botao.parent().find('[name=elaboracao]').removeClass('active').removeClass('btn-warning').addClass('btn-secondary').addClass('disabled');
         }
     }
 
     $(function() {
 
-        $('.enable-disable-btn').find('[name=disable]').click(function() {
+        $('.enable-disable-btn').find('[name=elaboracao]').click(function() {
             if ($(this).hasClass('active')) {
                 $(this).blur()
                 return false
             }
-            if (confirm('Deseja mudar para EM ELABORAÇÃO?')) {
-                $(this).removeClass('disabled').removeClass('btn-secondary').addClass('btn-danger').addClass('active').blur();
-                $(this).parent().find('[name=enable]').removeClass('active').removeClass('btn-success').addClass('btn-secondary').addClass('disabled')
+            if (confirm('Deseja mudar para EM ELABORAÇÂO?')) {
+                $(this).removeClass('disabled').removeClass('btn-secondary').addClass('btn-warning').addClass('active').blur();
+                $(this).parent().find('[name=desativada]').removeClass('active').removeClass('btn-danger').addClass('btn-secondary').addClass('disabled');
+                $(this).parent().find('[name=producao]').removeClass('active').removeClass('btn-success').addClass('btn-secondary').addClass('disabled');
             }
         })
 
-        $('.enable-disable-btn').find('[name=enable]').click(function() {
+        $('.enable-disable-btn').find('[name=producao]').click(function() {
             if ($(this).hasClass('active')) {
                 $(this).blur()
                 return false
             }
             if (confirm('Deseja mudar para EM PRODUÇÃO?')) {
                 $(this).removeClass('disabled').removeClass('btn-secondary').addClass('btn-success').addClass('active').blur();
-                $(this).parent().find('[name=disable]').removeClass('active').removeClass('btn-danger').addClass('btn-secondary').addClass('disabled')
+                $(this).parent().find('[name=desativada]').removeClass('active').removeClass('btn-danger').addClass('btn-secondary').addClass('disabled');
+                $(this).parent().find('[name=elaboracao]').removeClass('active').removeClass('btn-warning').addClass('btn-secondary').addClass('disabled');
+            }
+        })
+
+        $('.enable-disable-btn').find('[name=desativada]').click(function() {
+            if ($(this).hasClass('active')) {
+                $(this).blur()
+                return false
+            }
+            if (confirm('Deseja mudar para DESATIVADA?')) {
+                $(this).removeClass('disabled').removeClass('btn-secondary').addClass('btn-danger').addClass('active').blur();
+                $(this).parent().find('[name=producao]').removeClass('active').removeClass('btn-success').addClass('btn-secondary').addClass('disabled');
+                $(this).parent().find('[name=elaboracao]').removeClass('active').removeClass('btn-warning').addClass('btn-secondary').addClass('disabled');
             }
         })
 
