d2a097287c3733560b4def046bd954099da0c303
Masaki Kawabata Neto <kawabata@usp.br>
2020-10-30 13:11:03 -0300

    colocando icone de calendario

--- resources/views/chamados/partials/mostra-ano.blade.php
+++ resources/views/chamados/partials/mostra-ano.blade.php
@@ -1,7 +1,7 @@
 <div class="ml-auto">
     <div class="dropdown">
         <a class="btn btn-info dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            {{ session('ano') }}
+           <i class="far fa-calendar-alt mr-2"></i> {{ session('ano') }}
         </a>
 
         <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
