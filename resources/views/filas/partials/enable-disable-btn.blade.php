df445ef83975fff3d5f8e8280b85a5d291c473e5
Masaki Kawabata Neto <kawabata@usp.br>
2020-11-19 15:36:56 -0300

    desativando botao preselecionado

--- resources/views/filas/partials/enable-disable-btn.blade.php
+++ resources/views/filas/partials/enable-disable-btn.blade.php
@@ -1,15 +1,26 @@
+@section('styles')
+@parent
+{{-- https://stackoverflow.com/questions/50349017/how-can-i-change-cursor-for-disabled-button-or-a-in-bootstrap-4 --}}
+<style>
+    button:disabled {
+        cursor: not-allowed;
+        pointer-events: all !important;
+    }
+
+</style>
+@endsection
+
 {!! Form::open(['url'=>'filas/'.$fila->id, 'name' => 'form_estado']) !!}
 @method('put')
 @csrf
-
 <div class="btn-group enable-disable-btn">
-    <button type="submit" class="btn btn-sm {{($fila->estado == 'Em elaboração') ? 'btn-warning' : 'btn-secondary'}}" name="estado" value="Em elaboração">
+    <button type="submit" class="btn btn-sm {{($fila->estado == 'Em elaboração') ? 'btn-warning' : 'btn-secondary'}}" {{($fila->estado == 'Em elaboração') ? 'disabled' : ''}} name="estado" value="Em elaboração">
         Em elaboração
     </button>
-    <button type="submit" class="btn btn-sm {{($fila->estado == 'Em produção') ? 'btn-success' : 'btn-secondary'}}" name="estado" value="Em produção">
+    <button type="submit" class="btn btn-sm {{($fila->estado == 'Em produção') ? 'btn-success' : 'btn-secondary'}}" {{($fila->estado == 'Em produção') ? 'disabled' : ''}} name="estado" value="Em produção">
         Em produção
     </button>
-    <button type="submit" class="btn btn-sm {{($fila->estado == 'Desativada') ? 'btn-danger' : 'btn-secondary'}}" name="estado" value="Desativada">
+    <button type="submit" class="btn btn-sm {{($fila->estado == 'Desativada') ? 'btn-danger' : 'btn-secondary'}}" {{($fila->estado == 'Desativada') ? 'disabled' : ''}} name="estado" value="Desativada">
         Desativada
     </button>
 </div>
