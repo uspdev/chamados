56ca0abbccc8b0eacc2c33937421247626e437c7
Masaki Kawabata Neto <kawabata@usp.br>
2020-11-19 10:02:23 -0300

    fixando mensagem no rodape com autodismiss

--- resources/views/messages/flash.blade.php
+++ resources/views/messages/flash.blade.php
@@ -1,10 +1,18 @@
-    <div class="flash-message">
-        @foreach (['danger', 'warning', 'success', 'info'] as $msg)
-            @if(Session::has('alert-' . $msg))
-            
-            <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} 
-                <a href="#" class="close" data-dismiss="alert" aria-label="fechar">&times;</a>
-            </p>
-            @endif
-        @endforeach
-    </div>
+<div class="flash-message fixed-bottom w-75 ml-auto mr-auto">
+    @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+    @if(Session::has('alert-' . $msg))
+    <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }}
+        <a href="#" class="close" data-dismiss="alert" aria-label="fechar">&times;</a>
+    </p>
+    @endif
+    @endforeach
+</div>
+
+@section('javascripts_bottom')
+<script>
+    $(".flash-message").fadeTo(5000, 500).slideUp(500, function() {
+        $(".flash-message").slideUp(500);
+    });
+
+</script>
+@endsection
