b92901c98237d00b2db7e20c67f6ec0bdb286efd
Masaki Kawabata Neto <kawabata@usp.br>
2020-12-09 15:52:08 -0300

    tooltip

--- resources/views/setores/partials/edit-modal.blade.php
+++ resources/views/setores/partials/edit-modal.blade.php
@@ -1,3 +1,3 @@
-<button type="button" class="btn btn-sm btn-light text-primary" onclick="edit_form({{ $setor->id }})">
+<button type="button" class="btn btn-sm btn-light text-primary" onclick="edit_form({{ $setor->id }})" data-toggle="tooltip" title="Editar">
     <i class="fas fa-edit"></i>
 </button>
