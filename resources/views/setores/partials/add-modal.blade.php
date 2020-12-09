46f94abaec1a7abe33d49495635a8f19dfaadb84
Masaki Kawabata Neto <kawabata@usp.br>
2020-12-09 15:51:54 -0300

    tooltip

--- resources/views/setores/partials/add-modal.blade.php
+++ resources/views/setores/partials/add-modal.blade.php
@@ -1,3 +1,3 @@
-<button type="button" class="btn btn-sm btn-light text-primary" onclick="add_form({{ $setor->id }})">
+<button type="button" class="btn btn-sm btn-light text-primary" onclick="add_form({{ $setor->id }})" data-toggle="tooltip" title="Adicionar">
     <i class="fas fa-plus"></i>
 </button>
