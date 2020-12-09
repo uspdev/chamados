a7d9b23ff153bf5d32c8a00d15e937a3e74197b4
Masaki Kawabata Neto <kawabata@usp.br>
2020-12-09 15:52:25 -0300

    reorganizando autorização

--- resources/views/setores/partials/setor.blade.php
+++ resources/views/setores/partials/setor.blade.php
@@ -1,7 +1,12 @@
 {{-- Mostra o conteúdo de um setor --}}
-<a name="{{ \Str::lower($setor->sigla) }}" class="font-weight-bold">{{$setor->sigla}} - {{$setor->nome}}</a>
-@includewhen($user->is_admin,'setores.partials.edit-modal')
-@includewhen($user->is_admin, 'setores.partials.add-modal')
-<span class="badge badge-primary">{{ count($setor->users) }} pessoas</span>
-<span class="badge badge-success">{{ count($setor->filas) }} filas</span>
-@include('setores.partials.detalhes')
+<div>
+    <a name="{{ \Str::lower($setor->sigla) }}" class="font-weight-bold">{{$setor->sigla}} - {{$setor->nome}}</a>
+    @can('perfilAdmin')
+    @include('setores.partials.edit-modal')
+    @include('setores.partials.add-modal')
+    @include('setores.partials.btn-delete')
+    @endcan
+    <span class="badge badge-primary">{{ count($setor->users) }} pessoas</span>
+    <span class="badge badge-success">{{ count($setor->filas) }} filas</span>
+    @include('setores.partials.detalhes')
+</div>
