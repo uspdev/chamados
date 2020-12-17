bae3689c61389840e51937a1b2f2d128a430c895
Masaki Kawabata Neto <kawabata@usp.br>
2020-12-17 16:07:02 -0300

    valores padrão de config de filas

--- config/chamados.php
+++ config/chamados.php
@@ -1,7 +1,18 @@
 <?php
 
+$filas = [
+    'triagem' => 0, # 0: sem triagem, 1: triagem pelo gerente da fila
+    'visibilidade' => [
+        'alunos' => 0,
+        'servidores' => 1,
+        'setores' => 'todos', # todos ou interno
+    ],
+    'patrimonio' => 0,
+];
+
 return [
     'admins' => env('ADMINS'),
     'usar_replicado' => env('USAR_REPLICADO', true),
-    'upload_max_filesize' => env('UPLOAD_MAX_FILESIZE', '16M')*1024,
+    'upload_max_filesize' => env('UPLOAD_MAX_FILESIZE', '16M') * 1024,
+    'filas' => $filas,
 ];
