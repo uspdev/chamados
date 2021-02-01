08a7363837e2cf9f539b1f07379c471489d21a2f
Fernando G Moura <fgm@sc.usp.br>
2021-02-01 09:39:53 -0300

    WIP #181

--- resources/views/filas/partials/config.blade.php
+++ resources/views/filas/partials/config.blade.php
@@ -127,20 +127,39 @@
                     <input class="form-control col-12" type="text" name="config[status][select][]" value="{{ $input ?? '' }}">
                 @endforeach
             </div>
-            <div class="col-3" id="options_select_cor">
+            <div class="col-6" id="options_select_cor">
                 <label class="form-label">Cor:</label>
                 @foreach ($fila->config->status->select_cor as $input)
-                    <select name="config[status][select_cor][]" class="form-control col-12">
-                        <option value="">Selecione...</option>
-                        <option value="danger" class="bg-danger text-white" @if($input == 'danger') selected @endif>Danger</option>
-                        <option value="warning" class="bg-warning text-dark" @if($input == 'warning') selected @endif>Warning</option>
-                        <option value="primary" class="bg-primary text-white" @if($input == 'primary') selected @endif>Primary</option>
-                        <option value="secondary" class="bg-secondary text-white" @if($input == 'secondary') selected @endif>Secondary</option>
-                        <option value="success" class="bg-success text-white" @if($input == 'success') selected @endif>Success</option>
-                        <option value="info" class="bg-info text-white" @if($input == 'info') selected @endif>Info</div>
-                        <option value="dark" class="bg-dark text-white" @if($input == 'dark') selected @endif>Dark</div>
-                        <option value="white" class="bg-white text-dark" @if($input == 'white') selected @endif>White</div>
-                    </select>
+                    @php
+                    $id = rand();
+                    @endphp
+                    <div class="form-inline">
+                        <select name="config[status][select_cor][]" class="form-control col-6" id="select_{{ $id }}">
+                            <option value="">Selecione...</option>
+                            <option value="bg-danger text-white" class="bg-danger text-white" @if ($input == 'bg-danger text-white') selected @endif>
+                                Danger</option>
+                            <option value="bg-warning text-dark" class="bg-warning text-dark" @if ($input == 'bg-warning text-dark') selected @endif>
+                                Warning</option>
+                            <option value="bg-primary text-white" class="bg-primary text-white" @if ($input == 'bg-primary text-white') selected @endif>
+                                Primary</option>
+                            <option value="bg-secondary text-white" class="bg-secondary text-white" @if ($input == 'bg-secondary text-white') selected @endif>
+                                Secondary</option>
+                            <option value="bg-success text-white" class="bg-success text-white" @if ($input == 'bg-success text-white') selected @endif>
+                                Success</option>
+                            <option value="bg-info text-white" class="bg-info text-white" @if ($input == 'bg-info text-white') selected @endif>Info
+                            </option>
+                            <option value="bg-dark text-white" class="bg-dark text-white" @if ($input == 'bg-dark text-white') selected @endif>Dark
+                            </option>
+                            <option value="bg-white text-dark" class="bg-white text-dark" @if ($input == 'bg-white text-dark') selected @endif>White
+                            </option>
+                        </select>
+                        <span class="badge ml-2 {{ $input ?? '' }}" id="span_{{ $id }}">Teste</span>
+                        <span class="ml-2 {{ $input ?? '' }}" id="span2_{{ $id }}" data-toggle="tooltip" title="Teste">
+                            <i class="fas fa-circle"></i> </span>
+                        <button type="button" class="btn btn-sm btn-danger ml-2" id="btn_deleta_select"
+                            style="height: 22px; padding-top: 0px; padding-bottom: 0px; border-top-width: 0px; border-bottom-width: 0px; margin-bottom: 0px;"><i
+                                class="fa fa-minus"></i></button>
+                    </div>
                 @endforeach
             </div>
             @if (Gate::check('perfilAdmin'))
@@ -157,7 +176,7 @@
 </div>
 
 <div class="mt-3">
-    <input class="btn-sm btn-primary" type="submit" name="ok" value="ok">
+    <input class="btn-sm btn-primary" id="submit" type="submit" name="ok" value="ok">
 </div>
 {!! Form::close() !!}
 
@@ -165,12 +184,75 @@
     @parent
     <script>
         $(document).on('click', '#btn_adiciona_select', function() {
-            var novoItem =
-                '<input class="form-control col-12" type="text" name="config[status][select][]" value="">';
+            var novoItem = '<input class="form-control col-12" type="text" name="config[status][select][]" value="">';
             $("#options_select").append(novoItem);
-
-            var novoItemCor = '<select name="config[status][select_cor][]" class="form-control col-12"><option value="">Selecione...</option><option value="danger" class="bg-danger text-white">Danger</option><option value="warning" class="bg-warning text-dark">Warning</option><option value="primary" class="bg-primary text-white">Primary</option><option value="secondary" class="bg-secondary text-white">Secondary</option><option value="success" class="bg-success text-white">Success</option><option value="info" class="bg-info text-white">Info</div><option value="dark" class="bg-dark text-white">Dark</div><option value="white" class="bg-white text-dark">White</div></select>';
+            var id = Math.floor(Math.random() * 1000 + 1);
+            var novoItemCor = '<div class="form-inline">';
+            novoItemCor += '<select name="config[status][select_cor][]" class="form-control col-6" id="select_' + id + '">';
+            novoItemCor += '<option value="">Selecione...</option>';
+            novoItemCor += '<option value="bg-danger text-white" class="bg-danger text-white">Danger</option>';
+            novoItemCor += '<option value="bg-warning text-dark" class="bg-warning text-dark">Warning</option>';
+            novoItemCor += '<option value="bg-primary text-white" class="bg-primary text-white">Primary</option>';
+            novoItemCor += '<option value="bg-secondary text-white" class="bg-secondary text-white">Secondary</option>';
+            novoItemCor += '<option value="bg-success text-white" class="bg-success text-white">Success</option>';
+            novoItemCor += '<option value="bg-info text-white" class="bg-info text-white">Info</option>';
+            novoItemCor += '<option value="bg-dark text-white" class="bg-dark text-white">Dark</option>';
+            novoItemCor += '<option value="bg-white text-dark" class="bg-white text-dark">White</option>';
+            novoItemCor += '</select>';
+            novoItemCor += '<span class="badge ml-2" id="span_' + id + '">Teste</span>';
+            novoItemCor += '<span class="ml-2" data-toggle="tooltip" title="Teste" id="span2_' + id + '"> <i class="fas fa-circle"></i> </span>';
+            novoItemCor += '<button type="button" class="btn btn-sm btn-danger ml-2" id="btn_deleta_select" style="height: 22px; padding-top: 0px; padding-bottom: 0px; border-top-width: 0px; border-bottom-width: 0px; margin-bottom: 0px;"><i class="fa fa-minus"></i></button>';
+            novoItemCor += '</div>';
             $("#options_select_cor").append(novoItemCor);
+        });
+
+        $(document).on('click', '#btn_deleta_select', function() {
+            if (confirm("Tem certeza que quer apagar?")) {
+                var inputs = document.getElementsByName('config[status][select][]');
+                var selects = document.getElementsByName('config[status][select_cor][]');
+                var i = 0,
+                    j = 0,
+                    s;
+                selects.forEach(element => {
+                    if (element.parentElement == this.parentElement) {
+                        var node = element.parentElement;
+                        node.parentNode.removeChild(node);
+                        s = i;
+                    }
+                    i++;
+                });
+                inputs.forEach(element => {
+                    if (s == j) {
+                        var node = element;
+                        node.parentNode.removeChild(node);
+                    }
+                    j++
+                });
+            }
+        });
+
+        $(document).on('change', 'select', function(e) {
+
+            var optionSelected, otherOptionSelected, match = 0;
+            if (this.name == 'config[status][select_cor][]') {
+                optionSelected = $("option:selected", this).val();
+                $('#' + 'span_' + this.id.substr(7)).removeClass().addClass('badge ml-2 ' + optionSelected);
+                $('#' + 'span2_' + this.id.substr(7)).removeClass().addClass('ml-2 ' + optionSelected);
+            }
+            $('select').each(function(index, element) {
+                if (this.name == 'config[status][select_cor][]') {
+                    otherOptionSelected = $("option:selected", this).val();
+                    if (optionSelected == otherOptionSelected) {
+                        match++;
+                    }
+                }
+            });
+            if (match > 1) {
+                alert("Favor Selecionar cores diferentes para cada Status!");
+                $('#submit').hide();
+            } else {
+                $('#submit').show();
+            }
         });
 
     </script>
