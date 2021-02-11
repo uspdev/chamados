4213f1168d83e3be4ad95212b639a33cc833dd35
Masaki Kawabata Neto <kawabata@usp.br>
2021-02-11 16:41:20 -0300

    melhorando o acesso ao replicado

--- app/Models/Patrimonio.php
+++ app/Models/Patrimonio.php
@@ -11,9 +11,33 @@ class Patrimonio extends Model
 {
     use HasFactory;
 
+    protected $replicado = '';
+
+    /**
+     * Obtém dados do replicado, retorna e guarda no objeto
+     */
     public function replicado()
     {
-        return json_decode(json_encode(Bempatrimoniado::dump($this->numpat)));
+        if (empty($this->replicado)) {
+            $this->replicado = (object) Bempatrimoniado::dump($this->numpat);
+        }
+        return $this->replicado;
+    }
+
+    /**
+     * Retorna o número de patrimônio no formato 000.000000
+     */
+    public function numFormatado()
+    {
+        return str_pad(substr($this->numpat, 0, -6), 3, '0', STR_PAD_LEFT) . '.' . substr($this->numpat, strlen($this->numpat) - 6);
+    }
+
+    /**
+     * Obtém o nome completo (nompesttd) do responsável pelo patrimonio
+     */
+    public function responsavel($codpes)
+    {
+        return Pessoa::nomeCompleto($codpes);
     }
 
     /**
@@ -22,15 +46,5 @@ class Patrimonio extends Model
     public function chamados()
     {
         return $this->belongsToMany('App\Models\Chamado', 'chamado_patrimonio')->withTimestamps();
-    }
-
-    public function numFormatado()
-    {
-        return str_pad(substr($this->numpat, 0, -6), 3, '0', STR_PAD_LEFT) . '.' . substr($this->numpat, strlen($this->numpat) - 6);
-    }
-
-    public function responsavel($codpes)
-    {
-        return json_decode(json_encode(Pessoa::nomeCompleto($codpes)));
     }
 }
