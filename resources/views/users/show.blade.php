8c21f6b490cfceca8574741301a6e751fe918abc
Masaki Kawabata Neto <kawabata@usp.br>
2021-02-12 14:05:59 -0300

    ocultando coisas de admin

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,14 +1,19 @@
 @extends('master')
 
 @section('content')
-    @parent
+@parent
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="h4 mt-2"><a href="users">Usuários</a> <i class="fas fa-angle-right"></i> {{ $user->name }}
-                @if(Gate::check('perfilAdmin')) | @include('users.partials.btn-change-user')
-                @endif
-            </div> 
+<div class="row">
+    <div class="col-md-12">
+        <div class="h4 mt-2">
+            @if(Gate::check('perfilAdmin'))
+            <a href="users">Usuários</a> <i class="fas fa-angle-right"></i>
+            @endif
+            {{ $user->name }}
+            @if(Gate::check('perfilAdmin')) | @include('users.partials.btn-change-user')
+            @endif
+        </div>
+        <div class="ml-2">
             <div>Nome: {{ $user->name }}</div>
             <div>email: {{ $user->email }}</div>
             <div>telefone: {{ $user->telefone }}</div>
@@ -16,51 +21,58 @@
             <div>Admin: {{ $user->is_admin ? 'sim' : 'não' }}</div>
         </div>
     </div>
+</div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="h4 mt-2">Setores</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="h4 mt-2">Setores</div>
+        <div class="ml-2">
             @forelse($user->setores as $setor)
-                <div><a href="setores/#{{ strtolower($setor->sigla) }}">{{ $setor->sigla }}</a> - {{ $setor->nome }}</div>
+            <div><a href="setores/#{{ strtolower($setor->sigla) }}">{{ $setor->sigla }}</a> - {{ $setor->nome }}</div>
             @empty
-                Sem setores
+            Sem setores
             @endforelse
         </div>
-    </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="h4 mt-2">Filas</div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="h4 mt-2">Filas</div>
+        <div class="ml-2">
             @forelse($user->filas as $fila)
-                <div><a href="filas/{{ $fila->id }}">({{$fila->Setor->sigla}}) {{ $fila->nome }}</a> - {{ $fila->descricao }}</div>
+            <div><a href="filas/{{ $fila->id }}">({{$fila->Setor->sigla}}) {{ $fila->nome }}</a> - {{ $fila->descricao }}</div>
             @empty
-                Sem filas
+            Sem filas
             @endforelse
         </div>
     </div>
+</div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="h4 mt-2">Meus chamados</div>
-            @forelse($user->chamados()->wherePivot('papel', 'Autor')->get() as $chamado)
-                <div><a href="chamados/{{ $chamado->id }}">{{ $chamado->nro }}/{{ $chamado->created_at->year }}</a> -
-                    {{ $chamado->assunto }}</div>
-            @empty
-                Sem chamados
-            @endforelse
+<div class="row">
+    <div class="col-md-12">
+        <div class="h4 mt-2">Meus chamados</div>
+        <div class="ml-2">
+            <a href="chamados">Veja aqui</a>
         </div>
     </div>
+</div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="h4 mt-2">Chamados que observo</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="h4 mt-2">Chamados que observo</div>
+        <div class="ml-2">
             @forelse($user->chamados()->wherePivot('papel', 'Observador')->get() as $chamado)
-                <div><a href="chamados/{{ $chamado->id }}">{{ $chamado->nro }}/{{ $chamado->created_at->year }}</a> -
-                    {{ $chamado->assunto }}</div>
+            <div>
+                <a href="chamados/{{ $chamado->id }}">{{ $chamado->nro }}/{{ $chamado->created_at->year }}</a> -
+                {{ $chamado->assunto }}
+            </div>
             @empty
-                Sem chamados
+            Sem chamados
             @endforelse
         </div>
     </div>
+</div>
 
 @endsection
