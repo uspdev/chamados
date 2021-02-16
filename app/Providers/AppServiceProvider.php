79c508c9265db5d2e314c87246d4323a7f6bd6d7
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-02-16 14:03:26 -0300

    opção para forçar https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        if(config('app.env') === 'production') {
-            #\URL::forceScheme('https');
+        if(config('chamados.forcar_https')) {
+            \URL::forceScheme('https');
         }
     }
 }
--- config/chamados.php
+++ config/chamados.php
@@ -4,4 +4,5 @@ return [
     'admins' => env('ADMINS'),
     'usar_replicado' => env('USAR_REPLICADO', true),
     'upload_max_filesize' => (int) env('UPLOAD_MAX_FILESIZE', '16') * 1024,
+    'forcar_https' => env('FORCAR_HTTPS', false),
 ];
