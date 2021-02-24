1fbdd3e5af66fa250f8e986f0fd66a24e7c5037b
Masaki Kawabata Neto <kawabata@usp.br>
2021-02-24 14:09:52 -0300

    reorganizando fila para usar cards

--- resources/views/filas/partials/config.blade.php
+++ resources/views/filas/partials/config.blade.php
@@ -1,157 +1,173 @@
-<div class="font-weight-bold">
-    <i class="fas fa-cogs"></i> Configurações
-</div>
+@section('styles')
+@parent
+<style>
+    #card-fila-config {
+        border: 1px solid coral;
+        border-top: 3px solid coral;
+    }
 
-{!! Form::open(['url' => 'filas/' . $fila->id, 'name' => 'form_config']) !!}
-@method('put')
+</style>
+@endsection
 
-<div class="ml-2 mt-2">
-    <span class="font-weight-bold">Triagem</span>
-    @include('ajuda.filas.config-triagem')
-
-    <div class="ml-2">
-        <span class="text-muted mr-2">habilitar triagem:</span>
-
-        <div class="form-check form-check-inline">
-            <label class="form-check-label">
-                <input class="form-check-input" type="radio" name="config[triagem]" value="1" {{ $fila->config->triagem ? 'checked' : '' }}>
-                Sim
-            </label>
-        </div>
-        <div class="form-check form-check-inline">
-            <label class="form-check-label">
-                <input class="form-check-input" type="radio" name="config[triagem]" value="0" {{ $fila->config->triagem ? '' : 'checked' }}>
-                Não
-            </label>
-        </div>
+<div class="card mb-3" id="card-fila-config">
+    <div class="card-header">
+        <i class="fas fa-cogs"></i> Configurações
     </div>
-</div>
+    <div class="card-body">
 
-<div class="ml-2 mt-3">
-    <span class="font-weight-bold">Visibilidade</span>
-    @include('ajuda.filas.config-visibilidade')
+        {!! Form::open(['url' => 'filas/' . $fila->id, 'name' => 'form_config']) !!}
+        @method('put')
 
-    <div class="ml-2">
-        <span class="text-muted mr-2">pessoas:</span>
-        <div class="ml-3">
-            <div class="form-check form-check-inline">
-                <label class="form-check-label">
-                    <input disabled class="form-check-input" type="checkbox" name="config[visibilidade][alunos]" value="1" {{ $fila->config->visibilidade->alunos ? 'checked' : '' }}>
-                    alunos
-                </label>
-            </div>
-            <div class="form-check form-check-inline">
-                <label class="form-check-label">
-                    <input class="form-check-input" type="checkbox" name="config[visibilidade][servidores]" value="1" {{ $fila->config->visibilidade->servidores ? 'checked' : '' }}>
-                    servidores
-                </label>
-            </div>
-            <div class="form-check form-check-inline">
-                <label class="form-check-label">
-                    <input class="form-check-input" type="checkbox" name="config[visibilidade][todos]" value="1" {{ $fila->config->visibilidade->todos ? 'checked' : '' }}>
-                    todos (USP)
-                </label>
-            </div>
-            <br>
-            <div class="form-check form-check-inline">
-                <label class="form-check-label">
-                    <input class="form-check-input" type="checkbox" name="config[visibilidade][setor_gerentes]" value="1" {{ $fila->config->visibilidade->setor_gerentes ? 'checked' : '' }}>
-                    gerentes de setor
-                </label>
-            </div>
-            <div class="form-check form-check-inline">
-                <label class="form-check-label">
-                    <input class="form-check-input" type="checkbox" name="config[visibilidade][fila_gerentes]" value="1" {{ $fila->config->visibilidade->fila_gerentes ? 'checked' : '' }}>
-                    gerentes de fila
-                </label>
+        <div class="ml-2 mt-2">
+            <span class="font-weight-bold">Triagem</span>
+            @include('ajuda.filas.config-triagem')
+
+            <div class="ml-2">
+                <span class="text-muted mr-2">habilitar triagem:</span>
+
+                <div class="form-check form-check-inline">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="radio" name="config[triagem]" value="1" {{ $fila->config->triagem ? 'checked' : '' }}>
+                        Sim
+                    </label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="radio" name="config[triagem]" value="0" {{ $fila->config->triagem ? '' : 'checked' }}>
+                        Não
+                    </label>
+                </div>
             </div>
         </div>
-    </div>
-    <div>
-        <div class="btn-group ml-2">
-            <span class="text-muted mr-2">setores:</span>
-            <div class="form-check form-check-inline">
-                <label class="form-check-label">
-                    <input class="form-check-input" type="radio" name="config[visibilidade][setores]" value="interno" {{ $fila->config->visibilidade->setores == 'interno' ? 'checked' : '' }}>
-                    interno ({{ $fila->setor->sigla }})
-                </label>
-            </div>
-            <div class="form-check form-check-inline">
-                <label class="form-check-label">
-                    <input class="form-check-input" type="radio" name="config[visibilidade][setores]" value="todos" {{ $fila->config->visibilidade->setores == 'todos' ? 'checked' : '' }}>
-                    todos
-                </label>
-            </div>
-        </div>
-    </div>
-</div>
 
-<div class="ml-2 mt-3">
-    <span class="font-weight-bold">Patrimônio</span>
-    @include('ajuda.filas.config-patrimonio')
+        <div class="ml-2 mt-3">
+            <span class="font-weight-bold">Visibilidade</span>
+            @include('ajuda.filas.config-visibilidade')
 
-    <div class="ml-2">
-        <span class="text-muted mr-2">Obrigatório:</span>
-
-        <div class="form-check form-check-inline">
-            <label class="form-check-label">
-                <input class="form-check-input" type="radio" name="config[patrimonio]" value="1" {{ $fila->config->patrimonio ? 'checked' : '' }}>
-                Sim
-            </label>
-        </div>
-        <div class="form-check form-check-inline">
-            <label class="form-check-label">
-                <input class="form-check-input" type="radio" name="config[patrimonio]" value="0" {{ $fila->config->patrimonio ? '' : 'checked' }}>
-                Não
-            </label>
-        </div>
-    </div>
-</div>
-
-<div class="ml-2 mt-3">
-    <span class="font-weight-bold">Status</span>
-    @include('ajuda.filas.config-status')
-
-    <div class="ml-2">
-        <div class="form-group form-row">
-            <div class="col-12" id="options_select">
-                <label class="form-label">Select:</label>
-                <button type="button" class="btn btn-sm btn-primary ml-2 mb-2" id="btn_adiciona_select" style="height: 22px; padding-top: 0px; padding-bottom: 0px; border-top-width: 0px; border-bottom-width: 0px; margin-bottom: 0px;"><i class="fa fa-plus"></i></button>
-                @foreach ($fila->config->status as $input)
-                <div class="form-inline mb-2">
-                    <input class="form-control col-5" type="text" name="config[status][select][]" value="{{ $input->label ?? '' }}">
-                    @php
-                    $id = rand();
-                    @endphp
-                    <select name="config[status][select_cor][]" class="form-control col-3 ml-2" id="select_{{ $id }}">
-                        <option value="">Selecione...</option>
-                        <option value="danger" class="bg-danger text-dark" @if ($input->color == 'danger') selected @endif>Danger</option>
-                        <option value="warning" class="bg-warning text-dark" @if ($input->color == 'warning') selected @endif>Warning</option>
-                        <option value="primary" class="bg-primary text-white" @if ($input->color == 'primary') selected @endif>Primary</option>
-                        <option value="secondary" class="bg-secondary text-white" @if ($input->color == 'secondary') selected @endif>Secondary</option>
-                        <option value="success" class="bg-success text-white" @if ($input->color == 'success') selected @endif>Success</option>
-                        <option value="info" class="bg-info text-white" @if ($input->color == 'info') selected @endif>Info</option>
-                        <option value="dark" class="bg-dark text-white" @if ($input->color == 'dark') selected @endif>Dark</option>
-                        <option value="white" class="bg-white text-dark" @if ($input->color == 'white') selected @endif>White</option>
-                    </select>
-                    <div style="flex: 0 0 150px;">
-                        <span class="ml-2 badge badge-{{ $input->color ?? '' }}" id="span_{{ $id }}">Teste</span>
-                        <span class="ml-2 text-{{ $input->color ?? '' }}" id="span2_{{ $id }}"><i class="fas fa-circle"></i></span>
-                        <button type="button" class="btn btn-sm btn-danger ml-2" id="btn_deleta_select" data-toggle="tooltip" title="Remover" style="height: 22px; padding-top: 0px; padding-bottom: 0px; border-top-width: 0px; border-bottom-width: 0px; margin-bottom: 0px;">
-                            <i class="fa fa-minus"></i>
-                        </button>
+            <div class="ml-2">
+                <span class="text-muted mr-2">pessoas:</span>
+                <div class="ml-3">
+                    <div class="form-check form-check-inline">
+                        <label class="form-check-label">
+                            <input disabled class="form-check-input" type="checkbox" name="config[visibilidade][alunos]" value="1" {{ $fila->config->visibilidade->alunos ? 'checked' : '' }}>
+                            alunos
+                        </label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox" name="config[visibilidade][servidores]" value="1" {{ $fila->config->visibilidade->servidores ? 'checked' : '' }}>
+                            servidores
+                        </label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox" name="config[visibilidade][todos]" value="1" {{ $fila->config->visibilidade->todos ? 'checked' : '' }}>
+                            todos (USP)
+                        </label>
+                    </div>
+                    <br>
+                    <div class="form-check form-check-inline">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox" name="config[visibilidade][setor_gerentes]" value="1" {{ $fila->config->visibilidade->setor_gerentes ? 'checked' : '' }}>
+                            gerentes de setor
+                        </label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox" name="config[visibilidade][fila_gerentes]" value="1" {{ $fila->config->visibilidade->fila_gerentes ? 'checked' : '' }}>
+                            gerentes de fila
+                        </label>
                     </div>
                 </div>
-                @endforeach
+            </div>
+            <div>
+                <div class="btn-group ml-2">
+                    <span class="text-muted mr-2">setores:</span>
+                    <div class="form-check form-check-inline">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="radio" name="config[visibilidade][setores]" value="interno" {{ $fila->config->visibilidade->setores == 'interno' ? 'checked' : '' }}>
+                            interno ({{ $fila->setor->sigla }})
+                        </label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="radio" name="config[visibilidade][setores]" value="todos" {{ $fila->config->visibilidade->setores == 'todos' ? 'checked' : '' }}>
+                            todos
+                        </label>
+                    </div>
+                </div>
             </div>
         </div>
+
+        <div class="ml-2 mt-3">
+            <span class="font-weight-bold">Patrimônio</span>
+            @include('ajuda.filas.config-patrimonio')
+
+            <div class="ml-2">
+                <span class="text-muted mr-2">Obrigatório:</span>
+
+                <div class="form-check form-check-inline">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="radio" name="config[patrimonio]" value="1" {{ $fila->config->patrimonio ? 'checked' : '' }}>
+                        Sim
+                    </label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="radio" name="config[patrimonio]" value="0" {{ $fila->config->patrimonio ? '' : 'checked' }}>
+                        Não
+                    </label>
+                </div>
+            </div>
+        </div>
+
+        <div class="ml-2 mt-3">
+            <span class="font-weight-bold">Estados</span>
+            @include('ajuda.filas.config-status')
+            <button type="button" class="btn btn-sm btn-light text-primary" id="btn_adiciona_select"><i class="fas fa-plus"></i> Adicionar</button>
+
+            <div class="ml-2">
+                <div class="form-group form-row">
+                    <div class="col-12" id="options_select">
+                        @foreach ($fila->config->status as $input)
+                        <div class="form-inline mb-2">
+                            <input class="form-control col-5" type="text" name="config[status][select][]" value="{{ $input->label ?? '' }}">
+                            @php
+                            $id = rand();
+                            @endphp
+                            <select name="config[status][select_cor][]" class="form-control col-3 ml-2" id="select_{{ $id }}">
+                                <option value="">Selecione...</option>
+                                <option value="danger" class="bg-danger text-dark" @if ($input->color == 'danger') selected @endif>Danger</option>
+                                <option value="warning" class="bg-warning text-dark" @if ($input->color == 'warning') selected @endif>Warning</option>
+                                <option value="primary" class="bg-primary text-white" @if ($input->color == 'primary') selected @endif>Primary</option>
+                                <option value="secondary" class="bg-secondary text-white" @if ($input->color == 'secondary') selected @endif>Secondary</option>
+                                <option value="success" class="bg-success text-white" @if ($input->color == 'success') selected @endif>Success</option>
+                                <option value="info" class="bg-info text-white" @if ($input->color == 'info') selected @endif>Info</option>
+                                <option value="dark" class="bg-dark text-white" @if ($input->color == 'dark') selected @endif>Dark</option>
+                                <option value="white" class="bg-white text-dark" @if ($input->color == 'white') selected @endif>White</option>
+                            </select>
+                            <div style="flex: 0 0 150px;">
+                                <span class="ml-2 badge badge-{{ $input->color ?? '' }}" id="span_{{ $id }}">Teste</span>
+                                <span class="ml-2 text-{{ $input->color ?? '' }}" id="span2_{{ $id }}"><i class="fas fa-circle"></i></span>
+                                <button type="button" class="btn btn-sm btn-danger ml-2" id="btn_deleta_select" data-toggle="tooltip" title="Remover" style="height: 22px; padding-top: 0px; padding-bottom: 0px; border-top-width: 0px; border-bottom-width: 0px; margin-bottom: 0px;">
+                                    <i class="fa fa-minus"></i>
+                                </button>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-3">
+            <input class="btn-sm btn-primary" id="submit" type="submit" name="ok" value="Salvar Configurações">
+        </div>
+        {!! Form::close() !!}
     </div>
 </div>
 
-<div class="mt-3">
-    <input class="btn-sm btn-primary" id="submit" type="submit" name="ok" value="ok">
-</div>
-{!! Form::close() !!}
+
 
 @section('javascripts_bottom')
 @parent
--- resources/views/filas/partials/formulario.blade.php
+++ resources/views/filas/partials/formulario.blade.php
@@ -1,13 +1,31 @@
-<span class="font-weight-bold"><i class="fab fa-wpforms"></i> Formulário</span>
-<a href="{{ route('filas.createtemplate', $fila->id) }}" class="btn btn-light btn-sm text-primary"><i class="fas fa-edit"></i> Editar</a>
+@section('styles')
+@parent
+<style>
+    #card-fila-formulario {
+        border: 1px solid coral;
+        border-top: 3px solid coral;
+    }
 
-@includewhen('perfiladmin', 'filas.partials.template-btn-show-json-modal')
+</style>
+@endsection
 
-<div class="ml-2">
-    <strong>Label - Tipo - Visibilidade</strong>
-    <br>
-    @foreach(json_decode($fila->template) as $field=>$value)
-    {{ $value->label }} - {{ $value->type }} - {{ $value->can ?? 'todos' }}<br>
-    @endforeach
+<div class="card mb-3" id="card-fila-formulario">
+    <div class="card-header">
+        <i class="fab fa-wpforms"></i> Formulário
+        <span class="small">@include('ajuda.filas.formulario')</span>
+
+        <a href="{{ route('filas.createtemplate', $fila->id) }}" class="btn btn-light btn-sm text-primary">
+            <i class="fas fa-edit"></i> Editar
+        </a>
+        @includewhen(Gate::check('perfiladmin'), 'filas.partials.template-btn-show-json-modal')
+    </div>
+    <div class="card-body">
+        <div class="ml-2">
+            <strong>Label - Tipo - Visibilidade</strong>
+            <br>
+            @foreach(json_decode($fila->template) as $field=>$value)
+            {{ $value->label }} - {{ $value->type }} - {{ $value->can ?? 'todos' }}<br>
+            @endforeach
+        </div>
+    </div>
 </div>
-<br>
