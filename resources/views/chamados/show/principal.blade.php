dc751b792999e0f0dc0fb2b366fd28303986ec5f
Masaki Kawabata Neto <kawabata@usp.br>
2021-03-02 15:03:37 -0300

    renomeando

---
+++ resources/views/chamados/show/principal.blade.php
@@ -0,0 +1,30 @@
+@section('styles')
+  @parent
+  <style>
+    #card-principal-conteudo {
+      font-size: 1.1em !important;
+    }
+
+  </style>
+@endsection
+
+<div id="card-principal-conteudo">
+  <span class="text-muted">Criado por:</span>
+  @if ($autor)
+    {{ $autor->name }} ({{ $autor->setores()->wherePivot('funcao', '!=', 'Gerente')->first()->sigla ?? 'sem setor' }})
+    @include('chamados.show.user-detail', ['user'=>$autor])<br>
+  @else
+    <span class="text-danger">** Sem autor **</span>
+  @endif
+
+  <span class="text-muted">Criado em:</span> {{ Carbon\Carbon::parse($chamado->created_at)->format('d/m/Y H:i') }}<br>
+
+  @if (!is_null($chamado->fechado_em))
+    <span class="text-muted">Fechado em</span>:
+    {{ Carbon\Carbon::parse($chamado->fechado_em)->format('d/m/Y H:i') }}<br>
+  @endif
+
+  <span class="text-muted">Assunto:</span> {{ $chamado->assunto }}<br>
+  <span class="text-muted">Descrição:</span> {{ $chamado->descricao ?? '' }}<br>
+  <br>
+</div>
