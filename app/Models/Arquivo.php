e9fe5c45fa4e32f49aaef6ec43166dedfb655f32
Masaki Kawabata Neto <kawabata@usp.br>
2021-03-29 16:30:11 -0300

    criando metodos de relacionamento

--- app/Models/Arquivo.php
+++ app/Models/Arquivo.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Arquivo extends Model
 {
     use HasFactory;
+
+    /**
+     * relacionamento com chamado
+     */
+    public function chamado()
+    {
+        return $this->belongsTo(Chamado::class);
+    }
+
+    /**
+     * relacionamento com user
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
