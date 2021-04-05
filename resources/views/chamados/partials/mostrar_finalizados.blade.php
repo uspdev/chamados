57e776864be81c87cb0a93e4591badaea1792cd8
Masaki Kawabata Neto <kawabata@usp.br>
2021-04-05 19:37:21 -0300

    aplicando filtro de finalizados

---
+++ resources/views/chamados/partials/mostrar_finalizados.blade.php
@@ -0,0 +1,22 @@
+<div class="form-group form-check">
+  <label class="form-check-label">Mostrar chamados finalizados </label>
+  <input type="checkbox" id="mostrar_finalizados" {{ session('finalizado') ? 'checked':'' }} class="form-check-input ml-1">
+</div>
+
+@section('javascripts_bottom')
+  @parent
+  <script>
+    $(document).ready(function() {
+
+        $('#mostrar_finalizados').change(function(){
+            if (this.checked) {
+                window.location.href='chamados?finalizado=1'
+            } else {
+                window.location.href='chamados?finalizado=0'
+            }
+        })
+
+    })
+
+  </script>
+@endsection
