95a51edc574be016fd2787f3e6565e9bfc2169fc
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2022-02-10 17:12:36 -0300

    Up
    Removendo um TESTE que ficou pra trás

--- resources/views/chamados/show/editar-formulario-btn.blade.php
+++ resources/views/chamados/show/editar-formulario-btn.blade.php
@@ -6,7 +6,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalChamadoEdit">Chamado TESTE</h5>
+                <h5 class="modal-title" id="modalChamadoEdit">Chamado</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
