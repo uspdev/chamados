81e13d98aea3988b40e1fffcee6042e6def38b38
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2022-02-10 17:34:35 -0300

    Add function mudarInputTextarea

--- public/js/functions.js
+++ public/js/functions.js
@@ -25,3 +25,40 @@ function ordenarOpcoes(campo) {
     var valFirstOption = options.first().val();
     $('select[name="extras[' + campo + ']"] option[value=""]').insertBefore('select[name="extras[' + campo + ']"] option[value="' + valFirstOption + '"]');
 }
+
+/* @autor uspdev/alecosta 10/02/2022
+* Função que verifica se o tipo de campo adicionado na fila é caixa de seleção
+* Se for, muda tipo tipo de campo valor de input para textarea
+*/
+function mudarCampoInputTextarea(campo) {
+	var fieldTypeSelect = $('select[name="' + campo + '"]').find(":selected").val();
+  // se é caixa de seleção, muda o campo valor para textarea
+  if (fieldTypeSelect == 'select') {
+    $('input[name="' + campo.replace('][type]', '][value]') + '"]').each(function () {
+      var classe = $(this).attr('class');
+      var style = $(this).attr('style');
+      var name = $(this).attr('name');
+      var value = $(this).val();
+      var textbox = $(document.createElement('textarea'));
+      textbox.attr('class', classe);
+      textbox.attr('name', name);
+      textbox.attr('style', style);
+      textbox.val(value);
+      $(this).replaceWith(textbox);
+    });
+	// do contrário, volta o campo valor para input
+  } else {
+      $('textarea[name="' + campo.replace('][type]', '][value]') + '"]').each(function () {
+      var classe = $(this).attr('class');
+      var style = $(this).attr('style');
+      var name = $(this).attr('name');
+      var value = $(this).val();
+      var inputbox = $(document.createElement('input'));
+      inputbox.attr('class', classe);
+      inputbox.attr('name', name);
+      inputbox.attr('style', style);
+      inputbox.val(value);
+      $(this).replaceWith(inputbox);
+    });
+  }
+}
