95c5a06240e1794c6a5f2274b88d2938e227b685
Masaki Kawabata Neto <kawabata@usp.br>
2022-04-05 07:48:34 -0300

    typo

--- resources/views/ajuda/filas/config-visibilidade.blade.php
+++ resources/views/ajuda/filas/config-visibilidade.blade.php
@@ -1,18 +1,19 @@
 <a data-toggle="collapse" href="#ajuda_visibilidade">
-    <i class="fas fa-question-circle text-primary"></i>
+  <i class="fas fa-question-circle text-primary"></i>
 </a>
 <div class="collapse" id="ajuda_visibilidade">
-    <div class="card-body card-ajuda">
-        <b>Ajuda para visibilidade</b><br>
-        Em princípio somente os atendentes e gerentes da própria fila podem abrir chamados nela.
-        As opções de visibilidade controlam quem mais pode criar chamados nessa fila.<br>
-        Selecione os grupos que você quer autorizar.<br>
-        <div class="ml-3">
-        <b>servidores</b> - todos os servidores da unidade<br>
-        <b>todos (USP)</b> - qualquer pessoa que tenha senha única, de qualquer unidade<br>
-        <b>gerentes de setor</b> -  qualquer pessoa que esteja listada como gerente de um setor<br>
-        <b>gerentes de fila</b> -  qualquer pessoa que esteja listada como gerente de uma fila<br>
-        <b>SETOR *</b> - qualquer pessoa que esteja vinculada ao setor onde foi criada a fila<br>
-        <b>lista personalizada</b> - lista de "números USP" das pessoas autorizadas<br>
+  <div class="card-body card-ajuda">
+    <b>Ajuda para visibilidade</b><br>
+    Em princípio somente os atendentes e gerentes da própria fila podem abrir chamados nela.
+    As opções de visibilidade controlam quem mais pode criar chamados nessa fila.<br>
+    Selecione os grupos que você quer autorizar.<br>
+    <div class="ml-3">
+      <b>servidores</b> - todos os servidores da unidade<br>
+      <b>todos (USP)</b> - qualquer pessoa que tenha senha única, de qualquer unidade<br>
+      <b>gerentes de setor</b> - qualquer pessoa que esteja listada como gerente de um setor<br>
+      <b>gerentes de fila</b> - qualquer pessoa que esteja listada como gerente de uma fila<br>
+      <b>SETOR *</b> - qualquer pessoa que esteja vinculada ao setor onde foi criada a fila<br>
+      <b>lista personalizada</b> - lista de "números USP" das pessoas autorizadas<br>
     </div>
+  </div>
 </div>
