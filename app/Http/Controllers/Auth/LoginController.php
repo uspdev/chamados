511b84bfe136762392abf7a9633039899c6e6f2a
Masaki Kawabata Neto <kawabata@usp.br>
2022-05-06 14:59:33 -0300

    limpando sessão no logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -108,6 +108,8 @@ class LoginController extends Controller
     public function logout(Request $request)
     {
         Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
         return redirect('/');
     }
 }
