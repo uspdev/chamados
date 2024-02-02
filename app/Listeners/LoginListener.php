fe230334607d8acb32c2c2deb471a25c3861c3c4
Masaki K Neto <kawabata@usp.br>
2024-02-02 10:15:53 -0300

    corrigindo update last login

--- app/Listeners/LoginListener.php
+++ app/Listeners/LoginListener.php
@@ -23,9 +23,24 @@ class LoginListener
         foreach ($vinculos as $vinculo) {
             if ($setor = Setor::where('cod_set_replicado', $vinculo['codset'])->first()) {
                 Setor::vincularPessoa($setor, $user, mb_convert_case($vinculo['tipvin'], MB_CASE_TITLE));
-                $log .= 'codpes=' . $user->codpes . ',setor=' . $setor->sigla . ',vínculo=' . $vinculo['tipvin'] . ';';
+                $log .= 'update vinculo codpes=' . $user->codpes . ',setor=' . $setor->sigla . ',vínculo=' . $vinculo['tipvin'] . ';';
             }
         }
+
+        // vamos manter a configuracao antiga para compatibilidade retroativa
+        // mas deverá ser ajustado e removido as referências a "is_admin"
+        // vamos verificar no config se o usuário é admin
+        //$admins_codpes = explode(',', config('senhaunica.admins'));
+        if (in_array($user->codpes, config('senhaunica.admins'))) {
+            $user->is_admin = true;
+        }
+
+        // atualizando o last login do usuário
+        $user->last_login_at = now();
+        $user->save();
+
+        // será que precisa disso?
+        //session(['perfil' => 'usuario']);
 
         config('app.debug') && Log::info($log);
     }
