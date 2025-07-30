fe59129d8509d98c28fbd2b604f06e06d8e8bcd0
Masaki K Neto <kawabata@usp.br>
2025-07-30 11:21:49 -0300

    corrige erro de oauth

--- app/Listeners/LoginListener.php
+++ app/Listeners/LoginListener.php
@@ -1,32 +1,35 @@
 <?php
+
 namespace App\Listeners;
 
 use App\Models\Setor;
+use Uspdev\Replicado\Pessoa;
+use Uspdev\Replicado\Estrutura;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
-use Uspdev\Replicado\Pessoa;
 
 class LoginListener
 {
-    public function __construct()
-    {}
+    public function __construct() {}
 
     public function handle(Login $event)
     {
         $user = $event->user;
-        $vinculos = Pessoa::listarVinculosAtivos($user->codpes, false);
 
-        // atualizando base local (email e telefone com dados do oauth)
-        if ($userSenhaUnica = Socialite::driver('senhaunica')->user()) {
+        try {
+            $userSenhaUnica = Socialite::driver('senhaunica')->user();
             $userSenhaUnica = $userSenhaUnica->attributes;
             $user->telefone = $userSenhaUnica['telefone'];
             $user->email = $userSenhaUnica['email'];
+        } catch (\Exception $e) {
+            // mitigando erro do auth verifier fora do fluxo de login
         }
 
         $log = 'login listener:';
 
         // vincular a pessoa e o vinculo ao setor
+        $vinculos = Pessoa::listarVinculosAtivos($user->codpes, false);
         foreach ($vinculos as $vinculo) {
             if ($setor = Setor::where('cod_set_replicado', $vinculo['codset'])->first()) {
                 Setor::vincularPessoa($setor, $user, mb_convert_case($vinculo['tipvin'], MB_CASE_TITLE));
