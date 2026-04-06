0e755f783b892c28b4566cc7afdf35470ff2c203
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2026-04-06 16:45:14 -0300

    Deixando a exibição do local para outro momento

--- resources/views/chamados/show/user-detail.blade.php
+++ resources/views/chamados/show/user-detail.blade.php
@@ -5,7 +5,6 @@ $usar_foto = config('chamados.usar_foto');
 $setor_vinculo = $user->setores()->wherePivot('funcao', '!=', 'Gerente')->first();
 $setor_sigla = $setor_vinculo->sigla ?? 'sem setor';
 $setor_funcao = $setor_vinculo->pivot->funcao ?? '';
-$setor_local = '';
 
 $vinculos = array_values(array_filter(
   \Uspdev\Replicado\Pessoa::vinculos($user->codpes),
@@ -17,18 +16,6 @@ $ultimo_vinculo = !empty($vinculos) ? end($vinculos) : null; # Nos testes, traze
 $tipvinext = $ultimo_vinculo
   ? trim(explode('-', $ultimo_vinculo)[0])
   : $setor_funcao; # Se não houver vínculo, usa a funcao do setor
-
-if (config('chamados.usar_replicado') && $setor_vinculo && !empty($setor_vinculo->cod_set_replicado)) {
-  try {
-    $setor_replicado = \Uspdev\Replicado\Estrutura::dump($setor_vinculo->cod_set_replicado);
-    if (!empty($setor_replicado['codlocusp'])) {
-      $local = \Uspdev\Replicado\Estrutura::obterLocal($setor_replicado['codlocusp']);
-      $setor_local = $local['nomlocusp'] ?? '';
-    }
-  } catch (\Throwable $e) {
-    $setor_local = '';
-  }
-}
 
 if ($usar_foto) {
   $foto_base64 = \Uspdev\Wsfoto::obter((int) $user->codpes);
@@ -55,11 +42,6 @@ if ($usar_foto) {
       <div>
         Setor: {{ $setor_sigla }} - {{ $tipvinext }}
       </div>
-      @if ($setor_local)
-        <div>
-          Local do setor: {{ $setor_local }}
-        </div>
-      @endif
       <div>
         <i class="fas fa-envelope-square mr-2"></i> <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>
       </div>
