de9051ab96ea451d94ff8a79473c6108e4401cfb
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2026-04-09 17:50:09 -0300

    Campos extras no corpo do e-mail

--- resources/views/emails/dados_chamado.blade.php
+++ resources/views/emails/dados_chamado.blade.php
@@ -5,5 +5,30 @@
     para ({{ $chamado->fila->setor->sigla }}) {{ $chamado->fila->nome }}<br>
     Assunto: {{ $chamado->assunto }}<br>
     Descrição: {!! $chamado->descricao !!}<br>
+    @php
+        $template = json_decode($chamado->fila->template ?? '{}');
+        $extras = json_decode($chamado->extras ?? '{}');
+    @endphp
+    @if ($template && $extras)
+        <b>Campos extras:</b><br>
+        @foreach ($template as $field => $val)
+            @if (isset($extras->$field) && $extras->$field !== '')
+                @php
+                    $extraValue = $extras->$field;
+                    if (($val->type ?? null) === 'select') {
+                        $keyOption = (string) $extraValue;
+                        $extraValue = $val->value->$keyOption ?? $extraValue;
+                    } elseif (($val->type ?? null) === 'date') {
+                        try {
+                            $dateValue = Carbon\Carbon::parse($extraValue);
+                            $extraValue = ucfirst($dateValue->locale('pt_BR')->translatedFormat('l')) . ', ' . $dateValue->format('d/m/Y');
+                        } catch (\Exception $e) {
+                        }
+                    }
+                @endphp
+                <b>{{ $val->label ?? $field }}:</b> {{ $extraValue }}<br>
+            @endif
+        @endforeach
+    @endif
     Link direto: <a href="{{config('app.url')}}/chamados/{{$chamado->id}}">{{config('app.url')}}/chamados/{{$chamado->id}}</a><br>
 </div>
