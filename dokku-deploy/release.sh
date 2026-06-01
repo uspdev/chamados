3751b46caceddb289dfb3055d4c5165ad90f88f6
lmarthins0 <lmarthins@usp.br>
2026-06-01 07:18:00 -0300

    atualização dockerfile e remoção de pasta dokku-deploy

--- dokku-deploy/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views storage/logs bootstrap/cache
-chown -R www-data:www-data storage bootstrap/cache
-chmod -R 775 storage bootstrap/cache
-
-composer install --no-dev --optimize-autoloader --no-interaction --no-scripts
-php artisan migrate --force
